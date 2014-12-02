1e7d13bf27f50f49e91061065de450bc1f3a88d6
Ramon Boza <ramon.boza@king.com>
2014-12-02 15:26:28 +0100

    first commit

---
+++ src/main/java/csd/training/CITraining.java
@@ -0,0 +1,4 @@
+package csd.training;
+
+public class CITraining {
+}
---
+++ src/test/java/csd/training/AppTest.java
@@ -0,0 +1,12 @@
+package csd.training;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void testName() throws Exception {
+		Assert.assertTrue(true);
+	}
+}
